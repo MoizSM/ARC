1db46cb5fbdd2a9b8fcf88d5c4352dcb8a82af0d
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-22 22:55:38 +0000

    Update solution_25d8a9c8.py

--- src/solution_25d8a9c8.py
+++ src/solution_25d8a9c8.py
@@ -1,2 +1,10 @@
-import pandas as pd
-import numpy as np
+import sys
+import json
+
+try : 
+    with open(sys.argv[1] , 'r') as f:
+        data = json.load(f)
+        print(data)
+
+except:
+    print('Please Enter a JSON File Name')
