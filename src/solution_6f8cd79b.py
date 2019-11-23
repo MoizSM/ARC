c80b1b3891bdb75fbd9f97b8897bfd043f365dff
waseemshariff <45338857+waseemshariff126@users.noreply.github.com>
2019-11-23 11:19:44 +0000

    Update solution_6f8cd79b.py
    Updated the program with logic, still yet to work on it.

--- src/solution_6f8cd79b.py
+++ src/solution_6f8cd79b.py
@@ -1,3 +1,21 @@
+import sys
+import json
 import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
+
+
+def solve():
+    with open(sys.argv[1] , 'r') as f:
+        dataset = json.load(f)
+        data = np.asarray(dataset["test"][0]["input"])
+    a =[]
+    for i in range(len(data)):
+        if ((i==0) or i == len(data)-1):
+            data[i] = 8
+        else:
+            for j in data[i+1]:
+                if (j == 0):
+                    data[j] = 8
+    print(data)
+
+solve()
+
