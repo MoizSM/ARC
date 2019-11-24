3655ce8dd06d9fbacd792575b80a794bdc8df870
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-24 20:43:55 +0000

    Updates New Task File

---
+++ src/solution_794b24be.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import numpy as np
+
+def solve():
+    with open(sys.argv[1] , 'r') as f:
+        data = json.load(f)
+
+    grid = np.asarray(data['test'][0]['input'])
+    count = 0
+    for x in np.nditer(grid):
+        if x!=0 :
+            count = count + 1
+    print(grid)
+
+    for i in range(len(grid)):
+        for x in range(len(grid[i])):
+            if ( i == 0 and x < count ):
+                grid[i][x] = 2
+            else:
+                grid[i][x] = 0
+    
+    print(grid)
+        
+
+solve()
