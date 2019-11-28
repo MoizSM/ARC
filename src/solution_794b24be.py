2970dd5dac8ba71e55c07daae0e3c3ed140dab0b
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-28 01:00:02 +0000

    Updated solution_794b24be.py for training and testing set

--- src/solution_794b24be.py
+++ src/solution_794b24be.py
@@ -6,23 +6,27 @@ def solve():
     with open(sys.argv[1] , 'r') as f:
         data = json.load(f)
 
-    grid = np.asarray(data['test'][0]['input'])
-    count = 0
-    for x in np.nditer(grid):
-        if x!=0 :
-            count = count + 1
-    print(grid)
+    var = ['train' , 'test']
+    for z in var:
+        for n in range(len(data[z])):
+            grid = np.asarray(data[z][n]['input'])
 
-    for i in range(len(grid)):
-        for x in range(len(grid[i])):
-            if ( i == 0 and x < count ):
-                grid[i][x] = 2
-            else:
-                grid[i][x] = 0
-            if (count > 3):
-                grid[1][1]= 2
+            count = 0
+            for x in np.nditer(grid):
+                if x!=0 :
+                    count = count + 1
+            # print(grid)
+
+            for i in range(len(grid)):
+                for x in range(len(grid[i])):
+                    if ( i == 0 and x < count ):
+                        grid[i][x] = 2
+                    else:
+                        grid[i][x] = 0
+                    if (count > 3):
+                        grid[1][1]= 2
     
-    print(grid)
+            print(grid , '\n')
         
 
 solve()
