bd93019e8896a2c758266a5c012325698a3684d4
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-28 13:57:18 +0000

    Added comments to solution_794b24be.py

--- src/solution_794b24be.py
+++ src/solution_794b24be.py
@@ -2,30 +2,31 @@ import sys
 import json
 import numpy as np
 
-def solve():
+def solve(): #Function that contains the logic to solve the task
     with open(sys.argv[1] , 'r') as f:
-        data = json.load(f)
+        data = json.load(f) #Parsing the JSON file.
 
     var = ['train' , 'test']
+    #Running for all the training and testing inputs
     for z in var:
         for n in range(len(data[z])):
             grid = np.asarray(data[z][n]['input'])
-            count = 0
+            count = 0 #Count will contain the number of blue elements
             for x in np.nditer(grid):
                 if x!=0 :
-                    count = count + 1
+                    count = count + 1 #Iterating for every blue element.
 
             for i in range(len(grid)):
                 for x in range(len(grid[i])):
+                    #For loop for assigning the red (2) color elements.
                     if ( i == 0 and x < count ):
                         grid[i][x] = 2
                     else:
                         grid[i][x] = 0
-                    
                     if (count > 3):
                         grid[1][1]= 2
     
-            print(grid , '\n')
+            print(grid , '\n') #Printing the ouput grid
         
 
 solve()
