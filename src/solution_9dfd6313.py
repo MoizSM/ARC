bec911d8fbec60acc613cf160ed6b03109f7d6c7
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-30 02:29:22 +0000

    Update solution_9dfd6313.py

--- src/solution_9dfd6313.py
+++ src/solution_9dfd6313.py
@@ -6,3 +6,14 @@ import numpy as np
 def solve(): #Logic Funtion
     with open(sys.argv[1] , 'r') as f:
         data = json.load(f) #Parsing the JSON file
+
+        var = ['train' , 'test']
+        #Running for all the training and testing inputs
+        for x in var:
+            for n in range(len(data[x])):  
+                grid = np.asarray(data[x][n]['input'])
+                print(grid)
+                grid = np.rot90(grid)
+                print(grid)
+
+solve()
