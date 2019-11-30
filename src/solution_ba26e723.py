8090e8f8f638cf18eef9c0eaccf56e1eb87fba68
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-30 02:07:13 +0000

    Started solving solution_ba26e723.py

---
+++ src/solution_ba26e723.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import numpy as np 
+
+def solve(): #Logic Funtion
+    with open(sys.argv[1] , 'r') as f:
+        data = json.load(f) #Parsing the JSON file
+    
+    var = ['train' , 'test']
+    #Running for all the training and testing inputs
+    for x in var:
+        for n in range(len(data[x])):  
+            grid = np.asarray(data[x][n]['input'])
+            j = 0
+                    
