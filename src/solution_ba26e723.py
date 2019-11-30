4381103dacdeb1d1e241ee804466f23d4b501d46
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-30 02:17:29 +0000

    Completed solution for task ba26e723.json

--- src/solution_ba26e723.py
+++ src/solution_ba26e723.py
@@ -12,4 +12,12 @@ def solve(): #Logic Funtion
         for n in range(len(data[x])):  
             grid = np.asarray(data[x][n]['input'])
             j = 0
-                    
+            while( j < grid.shape[1]): #While j is less than the number of columns of the grid
+                for i in range(grid.shape[0]):
+                    if(grid[i][j] != 0): #If the row is not black (0)
+                        grid[i][j] = 6 #Change the color to pink (6)
+                j = j + 3 #Repeat the above for every 3rd column as per the challenge                    
+
+            print(grid , '\n') #Printing the output grid for the cases
+
+solve()
