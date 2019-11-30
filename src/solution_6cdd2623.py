76517862c34f6eca65e250cce248a6a817be976b
Moiz M <48330718+MoizSM@users.noreply.github.com>
2019-11-30 12:04:48 +0000

    Update solution_6cdd2623.py

--- src/solution_6cdd2623.py
+++ src/solution_6cdd2623.py
@@ -6,3 +6,11 @@ import numpy as np
 def solve(): #Logic Funtion
     with open(sys.argv[1] , 'r') as f:
         data = json.load(f) #Parsing the JSON file
+
+    grid = np.asarray(data['train'][0]['input'])
+    var = ['train' , 'test']
+    #Running for all the training and testing inputs
+    for x in var:
+        for n in range(len(data[x])):  
+            grid = np.asarray(data[x][n]['input'])
+            print(np.asarray(data[x][n]['output']) , '\n')
